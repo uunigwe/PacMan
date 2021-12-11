8d9e2f0d120cf47873aba4fa04e7f431f79b3154
Ugo <32546618+uunigwe@users.noreply.github.com>
2021-12-11 17:39:51 -0500

    Update PacMan.js

--- PacMan.js
+++ PacMan.js
@@ -1,7 +1,7 @@
 var pos = 0;
 const pacArray = [
-  ["./images/PacMan1.png", "./images/PacMan2.png"],
-  ["./images/PacMan3.png", "./images/PacMan4.png"],
+  ["PacManPictures/PacMan1.png", "PacManPictures/PacMan2.png"],
+  ["PacManPictures/PacMan3.png", "PacManPictures/PacMan4.png"],
 ];
 var direction = 0;
 const pacMen = [];
